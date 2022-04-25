eebb1caf85e41f8379d85f756ff1fdf33c699cb2
Mathias Hedqvist <dv19mht@cs.umu.se>
2022-04-25 20:24:31 +0200

    modified to only add epsilon transitions between terminal states in a and initial states in b

--- src/main/java/compositionalUtils/MyConcatenation.java
+++ src/main/java/compositionalUtils/MyConcatenation.java
@@ -21,10 +21,7 @@ import rationals.Builder;
 import rationals.NoSuchStateException;
 import rationals.State;
 import rationals.Transition;
-import rationals.properties.ContainsEpsilon;
-import rationals.properties.isEmpty;
 import rationals.transformations.BinaryTransformation;
-import rationals.transformations.Normalizer;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -57,76 +54,80 @@ import java.util.Set;
 public class MyConcatenation<L, Tr extends Transition<L>, T extends Builder<L, Tr, T>> implements BinaryTransformation<L, Tr, T> {
 
     public Automaton<L, Tr, T> transform(Automaton<L, Tr, T> a, Automaton<L, Tr, T> b) {
-        Automaton<L, Tr, T> ap;
+        Automaton<L, Tr, T> ap = a.clone();
+        Automaton<L, Tr, T> bp = b.clone();
 
-        /*
-        Only normalize if there is a terminal state
-         */
-        Set<State> terminals = a.terminals();
-        if (!terminals.isEmpty()) {
-            ap = new Normalizer<L, Tr, T>().transform(a);
-        } else {
-            ap = a.clone();
-        }
-        Automaton<L, Tr, T> bp = new Normalizer<L, Tr, T>().transform(b);
-        ContainsEpsilon<L, Tr, T> ce = new ContainsEpsilon<L, Tr, T>();
-        boolean ace = ce.test(a);
-        boolean bce = ce.test(b);
-        if (ap.states().size() == 0 && ace)
-            return b;
-        if (bp.states().size() == 0 && bce)
-            return a;
-        State junc = null; /* junction state */
         Automaton<L, Tr, T> c = new Automaton<>();
         Map<State, State> map = new HashMap<>();
+        Map<State, State> reversedMap = new HashMap<>();
+
         /* add all states from ap */
         Iterator<State> i1 = ap.states().iterator();
         while (i1.hasNext()) {
             State e = i1.next();
-            State n;
-            if (e.isInitial()) {
-                n = c.addState(true, ace && bce);
-            } else if(!e.isTerminal())
-                n = c.addState(false, e.isTerminal() && bce);
-            else
-                continue;
+            State n = c.addState(e.isInitial(), e.isTerminal());
             map.put(e, n);
+            reversedMap.put(n, e);
         }
+
         /* add states from bp */
         Iterator<State> i2 = bp.states().iterator();
         while (i2.hasNext()) {
             State e = i2.next();
-            State n;
-            if (!e.isInitial())  {
-                n = c.addState(false, e.isTerminal());
-                map.put(e, n);
-            }
+            State n = c.addState(e.isInitial(), e.isTerminal());
+            map.put(e, n);
+            reversedMap.put(n, e);
         }
+
         /* create junction state */
-        junc = c.addState(ace,bce);
         Iterator<Transition<L>> i3 = ap.delta().iterator();
         while (i3.hasNext()) {
             Transition<L> t = i3.next();
             try {
-                if (t.end().isTerminal())
-                    c.addTransition(new Transition<>(map.get(t.start()), t.label(), junc));
-                else
-                    c.addTransition(new Transition<>(map.get(t.start()), t.label(), map.get(t.end())));
+                c.addTransition(new Transition<>(map.get(t.start()), t.label(), map.get(t.end())));
+                if (t.end().isTerminal()) {
+                    /* add epsilon transitions to initial states in b */
+                    Set<State> bpInitials = bp.initials();
+                    for (State s : bpInitials) {
+                        c.addTransition(new Transition<>(map.get(t.end()), null, map.get(s)));
+                        map.get(s).setInitial(false);
+                    }
+                }
             } catch (NoSuchStateException x) {
+                throw new IllegalArgumentException("No such state");
             }
-
         }
+
         Iterator<Transition<L>> i4 = bp.delta().iterator();
         while (i4.hasNext()) {
             Transition<L> t = i4.next();
             try {
-                if (t.start().isInitial())
-                    c.addTransition(new Transition<>(junc, t.label(), map.get(t.end())));
-                else
-                    c.addTransition(new Transition<>(map.get(t.start()), t.label(), map.get(t.end())));
+                c.addTransition(new Transition<>(map.get(t.start()), t.label(), map.get(t.end())));
+                if (t.start().isInitial()) {
+                    /* add epsilon transitions to terminal states in a */
+                    Set<State> apTerminals = ap.terminals();
+                    for (State s : apTerminals) {
+                        c.addTransition(new Transition<>(map.get(s), null, map.get(t.start())));
+                        map.get(s).setTerminal(false);
+                    }
+                }
             } catch (NoSuchStateException x) {
+                throw new IllegalArgumentException("No such state");
             }
         }
+
+        Set<State> cInitials = c.initials();
+        for (State s : cInitials) {
+            State possAInitial = reversedMap.get(s);
+            if (bp.initials().contains(possAInitial) && !(ap.initials().contains(possAInitial))) {
+                s.setInitial(false);
+            }
+
+            if (ap.terminals().contains(possAInitial) && !(bp.terminals().contains(possAInitial))) {
+                s.setTerminal(false);
+            }
+        }
+
         return c;
     }
 }
