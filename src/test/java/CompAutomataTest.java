6fe3d95c764a8db830da3f759e0deed94e8ae1cb
Mathias Hedqvist <dv19mht@cs.umu.se>
2022-04-26 15:34:44 +0200

    removed invalid formulae tests and added model check to compare method

--- src/test/java/CompAutomataTest.java
+++ src/test/java/CompAutomataTest.java
@@ -4,6 +4,7 @@ import net.sf.tweety.logics.pl.syntax.Proposition;
 import net.sf.tweety.logics.pl.syntax.PropositionalSignature;
 import org.junit.Test;
 import rationals.Automaton;
+import rationals.properties.ModelCheck;
 import rationals.transformations.Reducer;
 import utils.AutomatonUtils;
 import utils.CompAutomatonUtils;
@@ -16,139 +17,69 @@ public class CompAutomataTest {
         boolean declare = true;
         PropositionalSignature ps = generateSignature();
 
-//        compareAutomataOnLDL("true", declare, ps);
-//
-//        compareAutomataOnLDL("false", declare, ps);
-//
-//        compareAutomataOnLDL("a", declare, ps);
+        compareAutomataOnLDL("tt", declare, ps);
 
+        compareAutomataOnLDL("ff", declare, ps);
 
-//        compareAutomataOnLDL("!true", declare, ps);
-//
-//        compareAutomataOnLDL("!false", declare, ps);
-//
-//        compareAutomataOnLDL("!(a)", declare, ps);
+        compareAutomataOnLDL("!(tt)", declare, ps);
 
-
-//        compareAutomataOnLDL("a && b", declare, ps);
-//
-//        compareAutomataOnLDL("a || b", declare, ps);
-//
-//        compareAutomataOnLDL("tt", declare, ps);
-//
-//        compareAutomataOnLDL("ff", declare, ps);
-//
-//        compareAutomataOnLDL("!(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("!(ff)", declare, ps);
+        compareAutomataOnLDL("!(ff)", declare, ps);
 
         //diamond formulae
-//        compareAutomataOnLDL("<true>(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("<true>(ff)", declare, ps);
-//
-//        compareAutomataOnLDL("<true>(a)", declare, ps);
-//
-//        compareAutomataOnLDL("<true>(!(a))", declare, ps);
-//
-//        compareAutomataOnLDL("<false>(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("<false>(ff)", declare, ps);
-//
-//        compareAutomataOnLDL("<false>(a)", declare, ps);
-//
-//        compareAutomataOnLDL("<false>(!(a))", declare, ps);
-//
-//        compareAutomataOnLDL("<a>(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("<a>(ff)", declare, ps);
-//
-//        compareAutomataOnLDL("<a>(a)", declare, ps);
-//
-//        compareAutomataOnLDL("<a>(!(a))", declare, ps);
+        compareAutomataOnLDL("<true>(tt)", declare, ps);
+
+        compareAutomataOnLDL("<true>(ff)", declare, ps);
+
+        compareAutomataOnLDL("<false>(tt)", declare, ps);
+
+        compareAutomataOnLDL("<false>(ff)", declare, ps);
+
+        compareAutomataOnLDL("<a>(tt)", declare, ps);
+
+        compareAutomataOnLDL("<a>(ff)", declare, ps);
 
         //box formulae
-//        compareAutomataOnLDL("[true](tt)", declare, ps);
-//
-//        compareAutomataOnLDL("[true](ff)", declare, ps); // end
-//
-//        compareAutomataOnLDL("[true](a)", declare, ps);
-//
-//        compareAutomataOnLDL("[true](!(a))", declare, ps);
-//
-//        compareAutomataOnLDL("[false](tt)", declare, ps);
-//
-//        compareAutomataOnLDL("[false](ff)", declare, ps);
-//
-//        compareAutomataOnLDL("[false](a)", declare, ps);
-//
-//        compareAutomataOnLDL("[false](!(a))", declare, ps);
-//
-//        compareAutomataOnLDL("[a](tt)", declare, ps);
-//
-//        compareAutomataOnLDL("[a](ff)", declare, ps);
-//
-//        compareAutomataOnLDL("[a](a)", declare, ps);
-//
-//        compareAutomataOnLDL("[a](!(a))", declare, ps);
-//
-//        compareAutomataOnLDL("[!(a)](tt)", declare, ps);
-//
-//        compareAutomataOnLDL("[!(a)](ff)", declare, ps);
-//
-//        compareAutomataOnLDL("[!(a)](a)", declare, ps);
-//
-//        compareAutomataOnLDL("[!(a)](!(a))", declare, ps);
+        compareAutomataOnLDL("[true](tt)", declare, ps);
+
+        compareAutomataOnLDL("[true](ff)", declare, ps); // end
+
+        compareAutomataOnLDL("[false](tt)", declare, ps);
+
+        compareAutomataOnLDL("[false](ff)", declare, ps);
+
+        compareAutomataOnLDL("[a](tt)", declare, ps);
+
+        compareAutomataOnLDL("[a](ff)", declare, ps);
+
+        compareAutomataOnLDL("[!(a)](tt)", declare, ps);
+
+        compareAutomataOnLDL("[!(a)](ff)", declare, ps);
 
         //star diamond formulae
-//        compareAutomataOnLDL("<(true)*>(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("<(true)*>(ff)", declare, ps);
-//
-//        compareAutomataOnLDL("<(true)*>(a)", declare, ps); // Not correct(?) (MyConcatenation)
-//
-//        compareAutomataOnLDL("<(true)*>(!(a))", declare, ps);
+        compareAutomataOnLDL("<(true)*>(tt)", declare, ps);
 
-//        compareAutomataOnLDL("<(false)*>(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("<(false)*>(ff)", declare, ps);
-//
-//        compareAutomataOnLDL("<(false)*>(a)", declare, ps); // Reduces alphabet to 'a'
-//
-//        compareAutomataOnLDL("<(false)*>(!(a))", declare, ps); // Reduces alphabet to 'b'
-//
-//        compareAutomataOnLDL("<(a)*>(tt)", declare, ps);
-//
-//        compareAutomataOnLDL("<(a)*>(ff)", declare, ps);
-//
-//        compareAutomataOnLDL("<(a)*>(a)", declare, ps); // Reduces alphabet to 'a'
-//
-//        compareAutomataOnLDL("<(a)*>(!(a))", declare, ps);
+        compareAutomataOnLDL("<(true)*>(ff)", declare, ps);
+
+        compareAutomataOnLDL("<(false)*>(tt)", declare, ps);
+
+        compareAutomataOnLDL("<(false)*>(ff)", declare, ps);
+
+        compareAutomataOnLDL("<(a)*>(tt)", declare, ps);
+
+        compareAutomataOnLDL("<(a)*>(ff)", declare, ps);
 
         //star box formulae
-//        compareAutomataOnLDL("[(true)*](tt)", declare, ps);
-//
-//        compareAutomataOnLDL("[(true)*](ff)", declare, ps);
-//
-//        compareAutomataOnLDL("[(true)*](a)", declare, ps);
-//
-//        compareAutomataOnLDL("[(true)*](!(a))", declare, ps);
-//
-//        compareAutomataOnLDL("[(false)*](tt)", declare, ps);
+        compareAutomataOnLDL("[(true)*](tt)", declare, ps);
 
-//        compareAutomataOnLDL("[(false)*](ff)", declare, ps);
+        compareAutomataOnLDL("[(true)*](ff)", declare, ps);
 
-//        compareAutomataOnLDL("[(false)*](a)", declare, ps);
-//
-//        compareAutomataOnLDL("[(false)*](!(a))", declare, ps);
-//
+        compareAutomataOnLDL("[(false)*](tt)", declare, ps);
+
+        compareAutomataOnLDL("[(false)*](ff)", declare, ps);
+
         compareAutomataOnLDL("[(a)*](tt)", declare, ps);
 
         compareAutomataOnLDL("[(a)*](ff)", declare, ps);
-
-        compareAutomataOnLDL("[(a)*](a)", declare, ps); // Reduces alphabet to 'a'
-
-        compareAutomataOnLDL("[(a)*](!(a))", declare, ps);
     }
 
     @Test
@@ -156,21 +87,31 @@ public class CompAutomataTest {
         boolean declare = true;
         PropositionalSignature ps = generateSignature();
 
-//        compareAutomataOnLTL("true", declare, ps); //<true>tt
-//
-//        compareAutomataOnLTL("false", declare, ps); //<false>tt
-//
-//        compareAutomataOnLTL("a", declare, ps); //<a>tt
+        compareAutomataOnLTL("true", declare, ps); //<true>tt
 
-//        compareAutomataOnLTL("!true", declare, ps); //<false>tt
-//
-//        compareAutomataOnLTL("!false", declare, ps); //<true>tt
+        compareAutomataOnLTL("false", declare, ps); //<false>tt
 
-//        compareAutomataOnLTL("!a", declare, ps); //<!a>tt
+        compareAutomataOnLTL("a", declare, ps); //<a>tt
 
-//        compareAutomataOnLTL("F(a)", declare, ps);
+        compareAutomataOnLTL("!true", declare, ps); //<false>tt
 
-//        compareAutomataOnLTL("G(a)", declare, ps); // Not correct (MyConcatenation)
+        compareAutomataOnLTL("!false", declare, ps); //<true>tt
+
+        compareAutomataOnLTL("!a", declare, ps); //<!a>tt
+
+        compareAutomataOnLTL("F(a)", declare, ps);
+
+        compareAutomataOnLTL("G(a)", declare, ps);
+
+//        compareAutomataOnLTL("a U b", declare, ps); // RE_TEST
+
+//        compareAutomataOnLTL("a W b", declare, ps); // RE_TEST
+
+//        compareAutomataOnLTL("a R b", declare, ps); // RE_TEST
+
+        compareAutomataOnLTL("a <-> b", declare, ps);
+
+        compareAutomataOnLTL("last", declare, ps);
     }
 
     private void compareAutomataOnLTL(String input, boolean declare, PropositionalSignature ps) {
@@ -184,7 +125,11 @@ public class CompAutomataTest {
         Automaton ldlf2dfa = AutomatonUtils.ldlf2Automaton(declare, ldlfFormula, ps);
         ldlf2dfa = new Reducer<>().transform(ldlf2dfa);
 
-        printComparison(ldlf2dfa, comp, ldlfFormula);
+        if (!(new ModelCheck<>().test(ldlf2dfa, comp))) {
+            printComparison(ldlf2dfa, comp, ldlfFormula);
+        } else {
+            System.out.println("Formula: " + ldlfFormula + " was OK");
+        }
     }
 
     private void compareAutomataOnLDL(String input, boolean declare, PropositionalSignature ps) {
@@ -197,7 +142,12 @@ public class CompAutomataTest {
         Automaton ldlf2dfa = AutomatonUtils.ldlf2Automaton(declare, ldlfFormula, ps);
         ldlf2dfa = new Reducer<>().transform(ldlf2dfa);
 
-        printComparison(ldlf2dfa, comp, ldlfFormula);
+        if (!(new ModelCheck<>().test(ldlf2dfa, comp))) {
+            printComparison(ldlf2dfa, comp, ldlfFormula);
+        } else {
+            System.out.println("Formula: " + ldlfFormula + " was OK");
+        }
+
     }
 
     private PropositionalSignature generateSignature() {
