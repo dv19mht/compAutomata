a674d98aa7f2d77ac5a6d1248fee09eedb083505
Mathias Hedqvist <dv19mht@cs.umu.se>
2022-05-10 13:32:38 +0200

    initial commit

---
+++ src/main/java/main/ExperimentResultWrapper.java
@@ -0,0 +1,33 @@
+package main;
+
+import rationals.Automaton;
+
+public class ExperimentResultWrapper {
+    private String inputFormula;
+    private String results;
+    private Automaton automaton;
+
+    public void setInputFormula(String inputFormula) {
+        this.inputFormula = inputFormula;
+    }
+
+    public void setAutomaton(Automaton automaton) {
+        this.automaton = automaton;
+    }
+
+    public void setResults(String results) {
+        this.results = results;
+    }
+
+    public String getInputFormula() {
+        return inputFormula;
+    }
+
+    public Automaton getAutomaton() {
+        return automaton;
+    }
+
+    public String getResults() {
+        return results;
+    }
+}
